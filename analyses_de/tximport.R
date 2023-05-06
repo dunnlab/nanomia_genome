b1ba74c6d45997da1fd2f1f7324afe57c5b68e62
Casey Dunn <casey.dunn@yale.edu>
2023-05-06 15:07:46 -0400

    cleaned up code a bit

--- analyses_de/tximport.R
+++ analyses_de/tximport.R
@@ -1,6 +1,4 @@
 library("tximport")
-#library("readr")
-#library("DESeq2")
 
 # Adapted from http://bioconductor.org/packages/devel/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#transcript-abundance-files-and-tximport-tximeta
 
@@ -12,7 +10,6 @@ files <- file.path("quant", dir_names, "quant.sf")
 names(files) <- samples$sample
 
 # In our case each entry is a gene, but we still need a map of genes to transcripts
-
 gene_names = c()
 for (i in 1:length(files)) {
 	q = read.table(files[[i]], header=TRUE, sep="\t")
@@ -27,7 +24,3 @@ tx2gene = data.frame(gene=gene_names, transcript=gene_names, stringsAsFactors=FA
 txi <- tximport(files, type="salmon", tx2gene=tx2gene)
 
 save(txi, samples, file = "txi.RData")
-
-#ddsNanomia <- DESeqDataSetFromTximport(txi,
-#	colData = samples,
-#	design = ~ tissue)
